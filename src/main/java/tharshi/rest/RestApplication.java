6f9d6264e672ccb04af95fb0cd5d19d270b8bf6d
Tharshikan <santhar2008@gmail.com>
2021-02-07 00:18:21 -0500

    added rest user consumer

--- src/main/java/tharshi/rest/RestApplication.java
+++ src/main/java/tharshi/rest/RestApplication.java
@@ -4,11 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestApplication {
     static final Logger log =
             LoggerFactory.getLogger(RestApplication.class);
+
+    @Bean
+    public Logger getLogger() {
+        return log;
+    }
+
 
     public static void main(String[] args) {
         log.info("Before Starting application");
--- src/main/java/tharshi/rest/controller/HomeController.java
+++ src/main/java/tharshi/rest/controller/HomeController.java
@@ -1,21 +1,42 @@
 package tharshi.rest.controller;
 
+
+import lombok.val;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import tharshi.rest.model.User;
+
+import java.net.URI;
 
 @RestController
 public class HomeController {
+    @Autowired
+    Logger log;
+
+    String URL = "https://api.github.com/user/";
 
     @GetMapping("/hello")
     public String getHello(@RequestParam(value = "name") String name) {
         return "<H1>Hello 22ss" + name + "</H1>";
     }
 
-    @GetMapping("/users")
-    public String getUsers() {
-        return "kjkj";
+    @GetMapping("/user/{id}")
+    public String getUsers(@PathVariable(value = "id") int id) {
+        log.info("kjj");
+        var restTemplate = new RestTemplate();
+        var url = URL + String.valueOf(id);
+        var user = restTemplate.getForObject(url, User.class);
+        log.info("lkjlj");
+        return "ljl";
+
+
     }
 
 
